a816d674e06ab9a016dcac70a2f2753450cb5d69
uttamjareda <2018eeb1192@iitrpr.ac.in>
2022-07-02 22:11:35 +0530

    improving bodyitem, adding props

--- src/Components/BodyFrame.js
+++ src/Components/BodyFrame.js
@@ -1,19 +1,39 @@
-import React, { Component } from 'react'
-import BodyItem from './BodyItem'
+import React, { Component } from "react";
+import BodyItem from "./BodyItem";
 
 export class BodyFrame extends Component {
   render() {
     return (
-      <div>
-        this is body frame
-        <BodyItem/>
-        <BodyItem/>
-        <BodyItem/>
-        <BodyItem/>
-        <BodyItem/>
+      <div className="container my-10 ">
+        <h2>this is a website on dogs</h2>
+
+        <div className="row">
+                <div className="col-md-4">
+                    <BodyItem title="mytitle" description="mydescription" />
+                </div>
+                <div className="col-md-4">
+                    <BodyItem title="mytitle" description="mydescription" />
+                </div>
+                <div className="col-md-4">
+                    <BodyItem title="mytitle" description="mydescription" />
+                </div>
+        </div>
+
+        <div className="row">
+                <div className="col-md-4">
+                    <BodyItem title="mytitle" description="mydescription" ImageUrl="https://cdn2.thedogapi.com/images/BJa4kxc4X.jpg" />
+                </div>
+                <div className="col-md-4">
+                    <BodyItem title="mytitle" description="mydescription" />
+                </div>
+                <div className="col-md-4">
+                    <BodyItem title="mytitle" description="mydescription" />
+                </div>
+        </div>
+
       </div>
-    )
+    );
   }
 }
 
-export default BodyFrame
+export default BodyFrame;
--- src/Components/BodyItem.js
+++ src/Components/BodyItem.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react'
+
+import React, { Component } from "react";
 
 export class BodyItem extends Component {
-  render() {
-    return (
-      <div>
-        this is bodyitem
-      </div>
-    )
+  
+    constructor(){
+        super();
+    }
+  
+  
+    render() {
+
+    let {title,description,ImageUrl}=this.props;
+
+    return (  
+        <div className="card my-3" style={{width: "18rem"}}>
+          <img src={ImageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+              {description}
+            </p>
+            <a href="/" className="btn btn-sm btn-primary">
+              Read more
+            </a>
+          </div>
+        </div>
+
+    );
   }
 }
 
-export default BodyItem
+export default BodyItem;
