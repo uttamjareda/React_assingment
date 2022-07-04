d0fe1d3b99e27ea6f7f356c01cef0c0544be1212
uttamjareda <2018eeb1192@iitrpr.ac.in>
2022-07-04 17:17:47 +0530

    buttons improved

--- src/Components/BodyFrame.js
+++ src/Components/BodyFrame.js
@@ -4,17 +4,17 @@ import Spinner from "./Spinner";
 
 export class BodyFrame extends Component {
   //   articles = [];
-// we have total 264 results, so limit=12 then 22 pages
+// we have total 264 results, we can change number of results per page using limit prop
   constructor() {
     super();
     this.state = {
       articles: [],
       load: false,
       page: 0,
-      limit: 12,
     };
   }
 
+//   fetching results from api and storing them in articles array by changing its state
   async componentDidMount() {
     let url = `https://api.thedogapi.com/v1/breeds?limit=${this.props.limit}&page=0&order=Asc`;
     this.setState({load:true})
@@ -25,7 +25,7 @@ export class BodyFrame extends Component {
   }
 
 
-
+//function to get next page
    handleNextClick = async () => {
     console.log("Next Clicked");
     let url = `https://api.thedogapi.com/v1/breeds?limit=${this.props.limit}&page=${this.state.page+1}&order=Asc`;
@@ -43,7 +43,7 @@ export class BodyFrame extends Component {
 
 
 
-
+// function to get previous page
   handlePrevClick = async () => {
     console.log("Prev Clicked");
     let url = `https://api.thedogapi.com/v1/breeds?limit=${this.props.limit}&page=${this.state.page-1}&order=Asc`;
@@ -64,12 +64,15 @@ export class BodyFrame extends Component {
         
         <div>
             <h2 className="text-center welcometext" style={{padding: "1rem"}}>Welcome to WoofWoof</h2>
+
+            {/* spinner component is displayed while waiting  */}
             {this.state.load && <Spinner/>}
 
             <div className="container bodyframe">
+                {/* iterating through articles array and when we have fetched data */}
                 {!this.state.load && this.state.articles.map((element) => {
                 return (
-                    <div className="container col" style={{padding: "1rem"}} key={element.id}>
+                    <div className="container" style={{padding: "1rem"}} key={element.id}>
                         <BodyItem
                         name={element.name}
                         height={element.height.metric}
@@ -86,8 +89,8 @@ export class BodyFrame extends Component {
             </div>
 
         <div className="container d-flex justify-content-between my-3">
-            <button disabled={this.state.page<=0} type="button" onClick={this.handlePrevClick}>&larr; Prev</button>
-            <button type="button" onClick={this.handleNextClick}>Next &rarr;</button>
+            <button disabled={this.state.page<=0} className="btn" type="button" onClick={this.handlePrevClick}>&larr; Prev</button>
+            <button className="btn" type="button" onClick={this.handleNextClick}>Next &rarr;</button>
           </div>
 
         </div>
